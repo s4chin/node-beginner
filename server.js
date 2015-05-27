c4a22d838f1b084b42b60dd59168e6fedb4abf60
Sachin Kelkar <sachinkel19@gmail.com>
2015-05-28 00:09:13 +0530

    Add request handlers

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-var http = require('http')
+var http = require('http');
+var url = require("url");
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("hello World");
-    response.end();
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(handle, pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("hello World");
+        response.end();
     }
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
